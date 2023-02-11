a98492035c939f532cddc397ede6951df8573a58
igorpavlneko <igor_pavlenko@yahoo.com>
2023-02-11 21:24:55 +0300

    feat [yearly-toggle]: Support hours added st-3

--- js/main.js
+++ js/main.js
@@ -1,29 +1,53 @@
 (function toggleHandler() {
-  window.addEventListener("load", function (event) {
+  window.addEventListener("load", function () {
     const basePrice = 65;
+
+    const priceMap = {
+      "online-support": 0.05,
+      "remote-support": 1,
+      "both-support": 0.08,
+      numberOfUsers: {
+        base: 1,
+        hundredPlus: 0.95,
+      },
+      responseTime: {
+        base: 1,
+        oneDay: 0.5,
+        eightHours: 1,
+        asap: 1.5,
+      },
+    };
 
     const pricePeriod = document.querySelector(".period-toggle-wrapper");
     const periodSpan = document.querySelector(".period-span");
     const configPricing = document.querySelector(".config-pricing");
-    let priceVal = (document.querySelector(".price-val").textContent =
-      basePrice);
+    let priceVal = document.querySelector(".price-val");
 
-    function updatePrice() {
-      priceVal.textContent = "55";
+    priceVal.textContent = basePrice;
+
+    function generatePrice(val) {
+      priceMap[val]
+    }
+
+    function updatePrice(val) {
+      console.log(val, ": ===:val");
+      priceVal.textContent = val;
     }
 
     pricePeriod.addEventListener("change", (e) => {
       if (e.target.classList.contains("period-toggle")) {
-        if (e.target.checked) periodSpan.textContent = "year";
-        else periodSpan.textContent = "month";
+        if (e.target.checked) {
+          periodSpan.textContent = "year";
+          updatePrice(45);
+        } else {
+          updatePrice(85);
+          periodSpan.textContent = "month";
+        }
       }
     });
 
     configPricing.addEventListener("change", (e) => {
-      if (e.target.id === "online-support") {
-        console.log("cuw");
-        updatePrice();
-      }
+      updatePrice(e.target.value);
     });
   });
 })();
